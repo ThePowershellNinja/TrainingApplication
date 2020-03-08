d43b347d2b6bff398dd0a30bbe67b43274c44cfc
ThePowershellNinja <micah.o.battin@gmail.com>
2020-03-07 19:18:22 -0600

    Removed FlowLayout

--- GetLearned.ps1
+++ GetLearned.ps1
@@ -14,30 +14,23 @@ $splitContainer = [System.Windows.Forms.SplitContainer]@{
     Dock = 'Fill'
     SplitterWidth = 5
     BorderStyle = 'Fixed3D'
-    SplitterDistance = 250
+    SplitterDistance = 6
+    IsSplitterFixed = $true
 }
 $splitContainer.Panel1.BackColor = "Gray"
 $splitContainer.Panel2.BackColor = "White"
 $main.Controls.Add($splitContainer)
 
-$flowPanel = [System.Windows.Forms.FlowLayoutPanel]@{
-    WrapContents = $false
-    Dock = 'Fill'
-    FlowDirection = 'BottomUp'
-    BackColor = 'Blue'
-}
-$splitContainer.Panel1.Controls.AddRange(@($flowPanel))
 
 $collapseExpandButton = [System.Windows.Forms.Button]@{
-    Location = '0,0'
+    #Location = '0,0'
     Size = '250,35'
     Text = '<'
-
+    Dock = 'Bottom'
 }
 
 $treeView = [System.Windows.Forms.TreeView]@{
-    Location = '0,0'
-    Size = '250,{0}' -f $flowPanel.ClientRectangle.Height
+    Size = '250,500'
     Dock = 'Fill'
 }
 function InitializeTreeView {
@@ -46,8 +39,7 @@ function InitializeTreeView {
     $treeView.Nodes.Add('Powershell Class')
     $treeView.EndUpdate()
 }
-
-$flowPanel.Controls.AddRange(@($collapseExpandButton,$treeView))
+$splitContainer.Panel1.Controls.AddRange(@($collapseExpandButton,$treeView))
 
 
 
